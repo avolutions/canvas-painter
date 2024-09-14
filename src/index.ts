1ae2bf7ec16cf282cab4c1f81f0099f7146db067
AlxndrVgt <alexander.vogt@avolutions.de>
2024-09-14 15:41:15 +0200

    Export all needed classes

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,5 @@
-export { Point } from './Point.js';
+export { Angle } from './Types/Angle.js';
+export { Canvas } from './Canvas.js';
+export { IShape } from './Shapes/IShape.js'
+export { Point } from './Types/Point.js';
 export { Rectangle } from './Shapes/Rectangle.js';
