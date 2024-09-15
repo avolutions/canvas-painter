177e39787f568351e26436dee80ce2d55fa6f673
AlxndrVgt <alexander.vogt@avolutions.de>
2024-09-15 20:51:41 +0200

    Add ESLint

---
+++ eslint.config.js
@@ -0,0 +1,30 @@
+import typescriptParser from "@typescript-eslint/parser";
+import js from "@eslint/js";
+import ts from 'typescript-eslint';
+
+export default [
+  {
+    files: ["src/**/*.{js,ts,jsx,tsx}"],
+    languageOptions: {
+      ecmaVersion: "latest",
+      sourceType: "module",
+      parser: typescriptParser,
+    },
+  },
+  js.configs.recommended,
+  ...ts.configs.recommended,
+  {
+    rules: {
+
+    }
+  },
+  {
+    ignores: [
+      "coverage/",
+      "dist/",
+      "docs/",
+      "node_modules/",
+      "tests/",
+    ]
+  },
+];
