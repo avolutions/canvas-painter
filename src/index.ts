a8693f3d2ecefc739aafee105995ca297f7aa976
AlxndrVgt <alexander.vogt@avolutions.de>
2024-09-26 17:24:54 +0200

    Add types folder in dist

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,18 @@
-export { Angle } from './types/Angle.js';
+/* Canvas */
 export { Canvas } from './Canvas.js';
+
+/* Shapes */
+export { IShape } from './shapes/IShape.js'
+export { Shape } from './shapes/Shape.js'
+export { Rectangle } from './shapes/Rectangle.js';
+
+/* Types */
+export { Angle } from './types/Angle.js';
+export { Point } from './types/Point.js';
+
+/* Options */
 export { CanvasOptions } from './options/CanvasOptions.js';
 export { RectangleOptions } from './options/RectangleOptions.js';
-export { IShape } from './shapes/IShape.js'
-export { Point } from './types/Point.js';
-export { Rectangle } from './shapes/Rectangle.js';
+
+/* Styles */
 export { RectangleStyle } from './styles/RectangleStyle.js';
