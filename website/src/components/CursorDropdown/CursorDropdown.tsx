50b2601d3bc3380cba67cded47a0caa2b4140086
AlxndrVgt <alexander.vogt@avolutions.de>
2024-10-31 15:12:04 +0100

    Add demo and playground to website

---
+++ website/src/components/CursorDropdown/CursorDropdown.tsx
@@ -0,0 +1,25 @@
+import React from 'react';
+import { Cursor } from '../../../node_modules/@avolutions/canvas-painter/dist/esm/index.js';
+
+interface CursorDropdownProps {
+  value: Cursor;
+  onChange: (newCursor: Cursor) => void;
+}
+
+const CursorDropdown: React.FC<CursorDropdownProps> = ({ value, onChange }) => {
+  const handleSelectChange = (e: React.ChangeEvent<HTMLSelectElement>) => {
+    onChange(e.target.value as Cursor);
+  };
+
+  return (
+    <select value={value} onChange={handleSelectChange}>
+      {Object.values(Cursor).map((cursorOption) => (
+        <option key={cursorOption as string} value={cursorOption as string}>
+          {cursorOption as string}
+        </option>
+      ))}
+    </select>
+  );
+};
+
+export default CursorDropdown;
---
+++ website/src/pages/demo.tsx
@@ -0,0 +1,30 @@
+import React, { useState } from 'react';
+import Layout from '@theme/Layout';
+import CanvasConfigurator from '../components/CanvasConfigurator/CanvasConfigurator';
+import styles from './demo.module.css';
+
+import { CanvasOptions, CanvasStyle } from '../../node_modules/@avolutions/canvas-painter/dist/esm/index.js';
+import CanvasDisplay from '../components/CanvasDisplay/CanvasDisplay';
+
+export default function Demo(): JSX.Element {
+  const [options, setOptions] = useState<CanvasOptions>(CanvasOptions.DefaultOptions);
+  const [style, setStyle] = useState<CanvasStyle>(CanvasStyle.DefaultStyle);
+
+  return (
+    <Layout>
+      <div className={styles.demoLayout}>
+        <aside className={styles.sidebar}>
+          <CanvasConfigurator
+            setOptions={setOptions}
+            setStyle={setStyle} />
+        </aside>
+
+        <main className={styles.content}>
+          <CanvasDisplay
+            options={options}
+            style={style} />
+        </main>
+      </div>
+    </Layout>
+  );
+};
