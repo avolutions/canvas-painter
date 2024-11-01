b58d69dd1df4b466d3fd34df629dcf35b3467786
AlxndrVgt <alexander.vogt@avolutions.de>
2024-11-01 19:10:00 +0100

    Add shape configurators

---
+++ website/src/components/LineConfigurator/LineConfigurator.tsx
@@ -0,0 +1,119 @@
+import React, { useState } from 'react';
+import { FiChevronDown, FiChevronUp } from 'react-icons/fi';
+import styles from '../../css/Configurator.module.css';
+import { Line } from '@avolutions/canvas-painter';
+
+interface LineConfiguratorProps {
+  line: Line;
+  onLineChange: (field: string, value: any) => void;
+}
+
+const LineConfigurator: React.FC<LineConfiguratorProps> = ({ line, onLineChange }) => {
+  const [isLineCollapsed, setIsLineCollapsed] = useState(true);
+  const [isLineStyleCollapsed, setIsLineStyleCollapsed] = useState(true);
+  const [isLineOptionsCollapsed, setIsLineOptionsCollapsed] = useState(true);
+
+  return (
+    <>
+      <div className={styles.collapsibleSection}>
+        <button
+          onClick={() => setIsLineCollapsed((prev) => !prev)}
+          className={`${styles.collapsibleHeader} ${styles.h3Header}`}
+        >
+          Line
+          {isLineCollapsed ? <FiChevronDown /> : <FiChevronUp />}
+        </button>
+        {!isLineCollapsed && (
+          <>
+            <div className={styles.formRow}>
+              <label>center.x</label>
+              <input
+                type="number"
+                value={line.start.x}
+                onChange={(e) => onLineChange('start.x', Number(e.target.value))}
+              />
+            </div>
+            <div className={styles.formRow}>
+              <label>center.y</label>
+              <input
+                type="number"
+                value={line.start.y}
+                onChange={(e) => onLineChange('start.y', Number(e.target.value))}
+              />
+            </div>
+            <div className={styles.formRow}>
+              <label>center.x</label>
+              <input
+                type="number"
+                value={line.end.x}
+                onChange={(e) => onLineChange('end.x', Number(e.target.value))}
+              />
+            </div>
+            <div className={styles.formRow}>
+              <label>center.y</label>
+              <input
+                type="number"
+                value={line.end.y}
+                onChange={(e) => onLineChange('end.y', Number(e.target.value))}
+              />
+            </div>
+
+            <div className={styles.collapsibleSection}>
+              <button
+                onClick={() => setIsLineStyleCollapsed((prev) => !prev)}
+                className={`${styles.collapsibleHeader} ${styles.h4Header}`}
+              >
+                Style
+                {isLineStyleCollapsed ? <FiChevronDown /> : <FiChevronUp />}
+              </button>
+              {!isLineStyleCollapsed && (
+                <>
+                  <div className={styles.formRow}>
+                    <label>color</label>
+                    <input
+                      type="color"
+                      value={line.style.color}
+                      onChange={(e) => onLineChange('style.color', e.target.value)}
+                    />
+                  </div>
+                  <div className={styles.formRow}>
+                    <label>width</label>
+                    <input
+                      type="number"
+                      value={line.style.width}
+                      min="0"
+                      onChange={(e) => onLineChange('style.width', Number(e.target.value))}
+                    />
+                  </div>
+                </>
+              )}
+            </div>
+            <div className={styles.collapsibleSection}>
+              <button
+                onClick={() => setIsLineOptionsCollapsed((prev) => !prev)}
+                className={`${styles.collapsibleHeader} ${styles.h4Header}`}
+              >
+                Options
+                {isLineOptionsCollapsed ? <FiChevronDown /> : <FiChevronUp />}
+              </button>
+              {!isLineOptionsCollapsed && (
+                <>
+                  <div className={styles.formRow}>
+                    <label>visible</label>
+                    <input
+                      type="checkbox"
+                      checked={line.options.visible}
+                      onChange={(e) => onLineChange('options.visible', e.target.checked)}
+                    />
+                  </div>
+                </>
+              )}
+            </div>
+          </>
+        )}
+      </div>
+    </>
+  );
+};
+
+export default LineConfigurator;
--- website/src/components/ShapeConfigurator/ShapeConfigurator.tsx
+++ website/src/components/ShapeConfigurator/ShapeConfigurator.tsx
@@ -1,8 +1,10 @@
 import React, { useEffect, useState } from 'react';
-import { FiChevronDown, FiChevronUp } from 'react-icons/fi';
-import styles from '../../css/Configurator.module.css';
 
-import { Canvas, Rectangle, RectangleOptions, RectangleStyle, Shape } from '../../../node_modules/@avolutions/canvas-painter/dist/esm/index.js';
+import { Canvas, Circle, Line, Rectangle, Shape, Square } from '@avolutions/canvas-painter';
+import RectangleConfigurator from '../RectangleConfigurator/RectangleConfigurator';
+import SquareConfigurator from '../SquareConfigurator/SquareConfigurator';
+import CircleConfigurator from '../CircleConfigurator/CircleConfigurator';
+import LineConfigurator from '../LineConfigurator/LineConfigurator';
 
 interface ShapeConfiguratorProps {
   canvas: Canvas
@@ -12,168 +14,51 @@ const ShapeConfigurator: React.FC<ShapeConfiguratorProps> = ({ canvas }) => {
   const [, setRenderTrigger] = useState(0); // Dummy state variable for forcing re-renders
 
   const [shapes, setShapes] = useState<Shape[]>([]);
-  const [rectangle, setRectangle] = useState<Rectangle>(new Rectangle(125, 50, 50, 50));
-
-  const [rectangleOptions, updateRectangleOptions] = useState<RectangleOptions>(RectangleOptions.DefaultOptions);
-  const [rectangleStyle, updateRectangleStyle] = useState<RectangleStyle>(RectangleStyle.DefaultStyle);
-
-  const [isRectangleCollapsed, setIsRectangleCollapsed] = useState(false);
-  const [isRectangleStyleCollapsed, setIsRectangleStyleCollapsed] = useState(true);
-  const [isRectangleOptionsCollapsed, setIsRectangleOptionsCollapsed] = useState(true);
+  const [rectangle, setRectangle] = useState<Rectangle>(new Rectangle(0, 0, 0, 0, 0, {}, {}));
+  const [square, setSquare] = useState<Square>(new Square(0, 0, 0, 0, {}, {}));
+  const [circle, setCircle] = useState<Circle>(new Circle(0, 0, 0, {}, {}));
+  const [line, setLine] = useState<Line>(new Line(0, 0, 0, 0, {}, {}));
 
   useEffect(() => {
-    setShapes((prevShapes) => [...prevShapes, rectangle]);
+    setShapes((prevShapes) => [...prevShapes, rectangle, square, circle, line]);
   }, []);
 
   useEffect(() => {
     shapes.forEach((shape) => canvas.watch(shape));
   }, [canvas, shapes]);
 
-  const handleRectangleChange = (field: string, value: any) => {
-    let target = rectangle;
+  const handleShapeChange = (shape: Shape, field: string, value: any) => {
+    let target = shape;
     const path = field.split('.');
 
-    // Traverse the path to the last property
     for (let i = 0; i < path.length - 1; i++) {
-      target = target[path[i]] as any;
+      target = target[path[i]];
     }
-
-    // Update the target property
-    const lastKey = path[path.length - 1];
-    target[lastKey] = value;
+    target[path[path.length - 1]] = value;
 
     // Trigger React to re-render
-    setRectangle(rectangle);
     setRenderTrigger((prev) => prev + 1); // Force a re-render
   };
 
   return (
     <>
       <h2>Shapes</h2>
-      <div className={styles.collapsibleSection}>
-        <button
-          onClick={() => setIsRectangleCollapsed((prev) => !prev)}
-          className={`${styles.collapsibleHeader} ${styles.h3Header}`}
-        >
-          Rectangle
-          {isRectangleCollapsed ? <FiChevronDown /> : <FiChevronUp />}
-        </button>
-        {!isRectangleCollapsed && (
-          <>
-            <div className={styles.formRow}>
-              <label>position.x</label>
-              <input
-                type="number"
-                value={rectangle.position.x}
-                onChange={(e) => handleRectangleChange('position.x', Number(e.target.value))}
-              />
-            </div>
-            <div className={styles.formRow}>
-              <label>position.y</label>
-              <input
-                type="number"
-                value={rectangle.position.y}
-                onChange={(e) => handleRectangleChange('position.y', Number(e.target.value))}
-              />
-            </div>
-            <div className={styles.formRow}>
-              <label>width</label>
-              <input
-                type="number"
-                value={rectangle.width}
-                onChange={(e) => handleRectangleChange('width', Number(e.target.value))}
-              />
-            </div>
-            <div className={styles.formRow}>
-              <label>height</label>
-              <input
-                type="number"
-                value={rectangle.height}
-                onChange={(e) => handleRectangleChange('height', Number(e.target.value))}
-              />
-            </div>
-            <div className={styles.formRow}>
-              <label>Rotation</label>
-              <input
-                type="range"
-                min="0"
-                max="360"
-                value={rectangle.rotation}
-                onChange={(e) => handleRectangleChange('rotation', Number(e.target.value))}
-              />
-              <span>{rectangle.rotation}°</span>
-            </div>
-
-            <div className={styles.collapsibleSection}>
-              <button
-                onClick={() => setIsRectangleStyleCollapsed((prev) => !prev)}
-                className={`${styles.collapsibleHeader} ${styles.h4Header}`}
-              >
-                Style
-                {isRectangleStyleCollapsed ? <FiChevronDown /> : <FiChevronUp />}
-              </button>
-              {!isRectangleStyleCollapsed && (
-                <>
-                  <div className={styles.formRow}>
-                    <label>color</label>
-                    <input
-                      type="color"
-                      value={rectangle.style.color}
-                      onChange={(e) => handleRectangleChange('style.color', e.target.value)}
-                    />
-                  </div>
-                  <div className={styles.formRow}>
-                    <label>border.color</label>
-                    <input
-                      type="color"
-                      value={rectangle.style.border.color}
-                      onChange={(e) => handleRectangleChange('style.border.color', e.target.value)}
-                    />
-                  </div>
-                  <div className={styles.formRow}>
-                    <label>border.width</label>
-                    <input
-                      type="number"
-                      value={rectangle.style.border.width}
-                      min="0"
-                      onChange={(e) => handleRectangleChange('style.border.width', Number(e.target.value))}
-                    />
-                  </div>
-                </>
-              )}
-            </div>
-            <div className={styles.collapsibleSection}>
-              <button
-                onClick={() => setIsRectangleOptionsCollapsed((prev) => !prev)}
-                className={`${styles.collapsibleHeader} ${styles.h4Header}`}
-              >
-                Options
-                {isRectangleOptionsCollapsed ? <FiChevronDown /> : <FiChevronUp />}
-              </button>
-              {!isRectangleOptionsCollapsed && (
-                <>
-                  <div className={styles.formRow}>
-                    <label>centered</label>
-                    <input
-                      type="checkbox"
-                      checked={rectangle.options.centered}
-                      onChange={(e) => handleRectangleChange('options.centered', e.target.checked)}
-                    />
-                  </div>
-                  <div className={styles.formRow}>
-                    <label>visible</label>
-                    <input
-                      type="checkbox"
-                      checked={rectangle.options.visible}
-                      onChange={(e) => handleRectangleChange('options.visible', e.target.checked)}
-                    />
-                  </div>
-                </>
-              )}
-            </div>
-          </>
-        )}
-      </div>
+      <RectangleConfigurator
+        rectangle={rectangle}
+        onRectangleChange={(field, value) => handleShapeChange(rectangle, field, value)}
+      />
+      <SquareConfigurator
+        square={square}
+        onSquareChange={(field, value) => handleShapeChange(square, field, value)}
+      />
+      <CircleConfigurator
+        circle={circle}
+        onCircleChange={(field, value) => handleShapeChange(circle, field, value)}
+      />
+      <LineConfigurator
+        line={line}
+        onLineChange={(field, value) => handleShapeChange(line, field, value)}
+      />
     </>
   );
 }
